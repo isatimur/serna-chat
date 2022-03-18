cbfb838ee053d941e0c6380df7d573ab84008101
tiisachenko <tiisachenko@dasreda.ru>
2022-03-18 16:34:52 +0300

    Initial commit

---
+++ src/test/java/com/timurisachenko/websocketchat/SpringWebsocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timurisachenko.websocketchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
